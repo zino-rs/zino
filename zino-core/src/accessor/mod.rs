f2e7d33553b893ba3c1beb28debc7f36efed5131
cosmod <panzan89@163.com>
2024-12-13 13:45:05 +0800

    Release version 0.29.2

--- zino-core/src/accessor/mod.rs
+++ zino-core/src/accessor/mod.rs
@@ -39,7 +39,7 @@
 //! # Examples
 //!
 //! ```rust
-//! use zino_core::{accessor::GloabalAccessor, error::Error, file::NamedFile};
+//! use zino_core::{accessor::GlobalAccessor, error::Error, file::NamedFile};
 //!
 //! async fn upload_file(file: NamedFile) -> Result<(), Error> {
 //!     let Some(operator) = GlobalAccessor::get("aliyun") else {
