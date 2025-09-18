2e9a73a5baab269141580cb187051834704b0243
stan <1786469296@qq.com>
2025-09-18 20:26:05 +0800

    add mod

--- crates/zino-ai/src/lib.rs
+++ crates/zino-ai/src/lib.rs
@@ -2,8 +2,11 @@
 #![doc(html_favicon_url = "https://zino.cc/assets/zino-logo.png")]
 #![doc(html_logo_url = "https://zino.cc/assets/zino-logo.svg")]
 
+pub mod client;
 pub mod completions;
 pub mod memory_base;
 pub mod provider;
 pub mod video_generation;
-pub mod client;
+
+pub mod memory;
+pub mod workflow;
