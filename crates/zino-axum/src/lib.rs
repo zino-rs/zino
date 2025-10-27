4e761e4d0fd1a0e0e02b27a1252747d2185688df
photino <panzan89@gmail.com>
2025-10-27 12:38:06 +0800

    Fix docs.rs documentation

--- crates/zino-axum/src/lib.rs
+++ crates/zino-axum/src/lib.rs
@@ -1,4 +1,3 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![doc = include_str!("../README.md")]
 #![doc(html_favicon_url = "https://zino.cc/assets/zino-logo.png")]
 #![doc(html_logo_url = "https://zino.cc/assets/zino-logo.svg")]
